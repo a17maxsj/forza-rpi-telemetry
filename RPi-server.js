42896921b2fff69e8c09cc7c387798a5bd7f1c03
Max <a17maxsj@student.his.se>
2019-12-05 16:43:00 +0100

    v1.0

--- RPi-server.js
+++ RPi-server.js
@@ -1,3 +1,4 @@
+
 var PORT = 35553;
 var HOST = '0.0.0.0';
 
@@ -5,37 +6,22 @@ var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
 var currentRpm = 0;
 var speed = 0;
-let obj = {};
+var gear = 0;
+
 
 //Initialize the lcd
-var Lcd = require('lcd'),
-    lcd = new Lcd({
-        rs: 26,
-        e: 19,
-        data: [13, 6, 5, 11],
-        cols: 16,
-        rows: 2
-    });
+var Lcd = require('lcd'), lcd = new Lcd({  rs: 26,  e: 19, data: [13, 6, 5, 11],  cols: 16, rows: 2 });
 
 server.on('listening', function() {
     var address = server.address();
-    console.log('> UDP Server listening on ' + address.address + ":" + address.port);
-
-
-});
+    console.log('> UDP Server listening on ' + address.address + ":" + address.port);});
 
 server.on('message', function(message, remote) {
 
     currentRpm = ~~ message.readFloatLE(16);
-    speed = message.readFloatLE(244);
-    speed =  speed * 3.6;
-    speed = ~~ speed;
-    /*
-    //Read rpm and round of decimals (DEBUG)
-    console.clear();
-    console.log("rpm: " + currentRpm);
-  */
-
+    speed =message.readFloatLE(244) * 3.6;
+    speed= ~~ speed;
+    gear = message.readUInt8(307);
 });
 server.bind(PORT, HOST);
 
@@ -45,12 +31,19 @@ lcd.on('ready', function() {
     setInterval(function() {
         lcd.clear();
         lcd.setCursor(0, 0);
-        lcd.print('forza-rpi');
+        if (gear === 0){
+                lcd.print('RPM:' + currentRpm + ' Gear:R');
+
+        }else{
+                lcd.print('RPM:' + currentRpm + ' Gear:' + gear);
+
+        }
         lcd.once('printed', function() {
             lcd.setCursor(0, 1);
-            lcd.print('R:' + currentRpm + '  S:' + speed + 'km/h');
+            lcd.print('Speed:' + speed + 'km/h');
 
         });
 
     }, 100);
-                
+});
+
